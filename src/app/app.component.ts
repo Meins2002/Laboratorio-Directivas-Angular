29bcaec20f1e6cd8ec367e3ec7e9f191b01621a1
James Florez <jandrey2002@gmail.com>
2024-02-14 15:54:33 -0500

    Agregada funcionalidad de resaltar elementos pares en la lista

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-directives';
+  public muestras = [
+    'Muestra 1',
+    'Muestra 2',
+    'Muestra 3',
+    'Muestra 4',
+    'Muestra 5',
+  ]
+
+}
